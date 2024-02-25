6b7d2385573b293def33529e1a507a36406a1ede
xick <>
2024-02-25 01:19:13 +0100

    add possibilty to set wifi remotely

--- OXOTP/screen3.h
+++ OXOTP/screen3.h
@@ -1,39 +1,100 @@
 void Wifi_screen() {
 
-  WiFi.mode(WIFI_AP);
 
-  //M5.Lcd.fillRect(0, 17, 160, 63, BLACK);
-  M5.Lcd.fillScreen(TFT_BLACK);
-  M5.Lcd.drawXBitmap(60, 22, wait_icon, wait_icon_width, wait_icon_height, TFT_WHITE, TFT_BLACK);      // wait icon
+  //----- generate SSID for wifi AP
+  char AP_ssid[13];
 
-
-  String pass_gen = "";
-  char password[9];
-  char ssid[13];
-
-
-  // //----- generate password for wifi
-  // for (int i = 0; i < 8; i++) {
-  //   pass_gen += rondom_letters[random(0, rondom_letters.length() - 1)];
-  // }
-  pass_gen = "1A2B3C4D"; // static
-  
-  pass_gen.toCharArray(password, 9);
-
-
-  //----- generate SSID for wifi
   String macaddr = String(WiFi.macAddress());
   macaddr.replace(":", "");
   macaddr = macaddr.substring(6);
   String ssid_mac = "OXOTP_" + macaddr;
-  ssid_mac.toCharArray(ssid, 13);
+  ssid_mac.toCharArray(AP_ssid, 13);
+
+  String pass_gen = "";
+  
+  //----- generate password for wifi
+  for (int i = 0; i < 8; i++) {
+    pass_gen += rondom_letters[random(0, rondom_letters.length() - 1)];
+  }
+  
+
+  String pass_static = ""; // Default password
+  pass_static = NVS.getString("wifi_password");
+  if (pass_static == "") {
+    pass_static = "1A2B3C4D5E"; // fallback to an unsecure password
+  }
+
+  //0 : random password mode, 1 : static password mode
+  passwordMode = NVS.getInt("passwordMode");
 
 
-  //----- set SSID and PASSWORD
+  Wifi_Mode = NVS.getString("wifiMode"); // Default mode
+  // if the mode is not set, default to AP mode
+  if (Wifi_Mode == "") {
+    Wifi_Mode = "AP";
+  }
 
-  WiFi.softAP(ssid, password);
 
-  Serial.println("AP started, password: " + pass_gen);
+  if (Wifi_Mode == "AP") {
+    Wifi_SSID = ssid_mac;
+  } else {
+    Wifi_SSID = NVS.getString("wifi_ssid");
+  }
+  
+
+
+  // Use the saved settings
+  if (passwordMode == 0 && Wifi_Mode == "AP") {
+    Serial.println("Using random password mode");
+    Wifi_PASSWORD = pass_gen;
+  } else {
+    Serial.println("Using static password mode");
+    Wifi_PASSWORD = pass_static;
+  }
+
+  char password[Wifi_PASSWORD.length() + 1];
+  Wifi_PASSWORD.toCharArray(password, Wifi_PASSWORD.length() + 1);
+
+  if (Wifi_Mode == "AP") {
+    Serial.println("Starting in AP mode");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(AP_ssid, password);
+  } 
+  else if (Wifi_Mode != "AP" && Wifi_SSID != "") {
+    Serial.println("Starting in STA mode");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(Wifi_SSID, password);
+    Serial.println("Try Connecting to " + Wifi_SSID + " with password " + password);
+
+    // Wait for the connection to complete
+    unsigned long startTime = millis();
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(250);
+      // If the connection doesn't complete within 10 seconds, break the loop
+      if (millis() - startTime > 10000) {
+        break;
+      }
+    }
+    Serial.println("Connection status: " + String(WiFi.status()));
+    // print the ip address in the serial monitor and in the screen
+    Serial.println("Connected to " + Wifi_SSID + " with IP: " + WiFi.localIP().toString());
+
+  }
+  
+
+  // If the connection failed, fallback to AP mode
+  if (Wifi_Mode != "AP" && WiFi.status() != WL_CONNECTED) {
+    Serial.println("Failed to connect to the specified network. Falling back to AP mode.");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(AP_ssid, password);
+    Serial.println("AP mode started, ssid: " + String(AP_ssid) + ", password: " + password);
+  }
+
+
+
+  //M5.Lcd.fillRect(0, 17, 160, 63, BLACK);
+  M5.Lcd.fillScreen(TFT_BLACK);
+  M5.Lcd.drawXBitmap(60, 22, wait_icon, wait_icon_width, wait_icon_height, TFT_WHITE, TFT_BLACK);      // wait icon
 
   M5.Lcd.fillRect(60, 22, wait_icon_width, wait_icon_height, BLACK);
   M5.Lcd.setFreeFont(&beta10pt7b);
@@ -41,10 +102,17 @@ void Wifi_screen() {
   M5.Lcd.print ("CONNECT TO:");
   M5.Lcd.setFreeFont(&beta8pt7b);
   M5.Lcd.setCursor(10, 64);
-  M5.Lcd.print (ssid_mac);
-  M5.Lcd.setCursor(10, 90);
-  M5.Lcd.print (pass_gen);
+  M5.Lcd.print (Wifi_SSID);
 
+
+  M5.Lcd.setCursor(10, 90);
+  // we don't want to show the password in the screen if not in AP mode
+  if (Wifi_Mode == "AP") {
+    M5.Lcd.print (Wifi_PASSWORD);
+  } else {
+    // print the ip address in the screen
+    M5.Lcd.print (WiFi.localIP().toString());
+  }
 
   //----- start server
   server.enableCORS();
@@ -351,6 +419,67 @@ void Wifi_screen() {
     serializeJson(jsondata, dataout);
 
     server.send(200, "text/html", dataout);
+  });
+
+
+  // -- Set WiFi endpoint. Payload is a JSON object with the following fields:
+  
+    // "mode" (string): "AP" or "STA"
+    // "ssid" (string): SSID of the network to connect to
+    // "password" (string): Password of the network to connect to or the password for the AP
+    // "passwordMode" (bool): true or false
+
+  
+
+  server.on("/setWifi", HTTP_OPTIONS, []() {
+    server.send(200);
+  });
+
+server.on("/setWifi", HTTP_POST, []() {
+    previousMillis = millis();
+    jsondata.clear();
+    deserializeJson(jsondata, server.arg(0));
+
+    if (jsondata.containsKey("mode")) {
+        String _mode = jsondata["mode"];
+        NVS.setString("wifiMode", _mode);
+        Wifi_Mode = _mode;
+    }
+
+    if (jsondata.containsKey("ssid")) {
+        String _ssid = jsondata["ssid"];
+        NVS.setString("wifi_ssid", _ssid);
+        Wifi_SSID = _ssid;
+    }
+
+    if (jsondata.containsKey("password")) {
+        String _pass = jsondata["password"];
+        NVS.setString("wifi_password", _pass);
+        Wifi_PASSWORD = _pass;
+    }
+
+    if (jsondata.containsKey("passwordMode")) {
+        int _passMode = jsondata["passwordMode"];
+        NVS.setInt("passwordMode", _passMode);
+        passwordMode = _passMode;
+    }
+
+    server.send(200, "text/html", "OK");
+});
+
+// endpoint for get the current wifi settings (mode, ssid, passwordMode)
+  server.on("/getWifi", HTTP_GET, []() {
+      previousMillis = millis();
+      jsondata.clear();
+
+      jsondata["mode"] = NVS.getString("wifiMode");
+      jsondata["ssid"] = NVS.getString("wifi_ssid");
+      jsondata["passwordMode"] = NVS.getInt("passwordMode", 1);
+
+      String dataout = "";
+      serializeJson(jsondata, dataout);
+
+      server.send(200, "text/html", dataout);
   });
 
 
--- OXOTP/variable_runtime.h
+++ OXOTP/variable_runtime.h
@@ -11,3 +11,7 @@ unsigned long previousMillis = 0;
 int menu_index = 0;
 
 String pin_UNLOCK = "";
+int passwordMode = 0; // 0 random, 1 fixed
+String Wifi_Mode = "AP"; // Default mode
+String Wifi_SSID = "";
+String Wifi_PASSWORD = "";
